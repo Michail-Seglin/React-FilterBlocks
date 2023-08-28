edcb3f5f7f8d6cf4c367fda0c8a25df3895e0cb0
Mikhail <mihailsavuskin13@gmail.com>
2023-08-28 21:48:02 +0300

    useParams

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import JobPage from './page/JobPage/JobPage'
+import { Route, Routes } from 'react-router-dom'
+import VacancyPage from './page/VacancyPage/VacancyPage'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path={'/'} element={<JobPage />} />
+        <Route path={'/vacancy/:id'} element={<VacancyPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
